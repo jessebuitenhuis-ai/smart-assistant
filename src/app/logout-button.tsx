a04f4022cdaa91d2f5c46e693652a25fcb172306
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2025-10-05 11:03:05 +0200

    remove old components

--- src/app/logout-button.tsx
+++ src/app/logout-button.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import logout from "@/actions/auth/logout";
-import Button from "@/ui/button";
+import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
 
 export default function LogoutButton() {
   const [pending, startTransition] = useTransition();
   const logoutAction = () => startTransition(() => logout());
   return (
-    <Button onClick={logoutAction} disabled={pending}>
-      {pending ? "Logging out..." : "Logout"}
+    <Button onClick={logoutAction} pending={pending} pendingEl="Logging out...">
+      Logout
     </Button>
   );
 }
